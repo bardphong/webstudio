3cf3f544dc981a428fde3c9e73c4aaca9f082794
bardphong <leanjpistol2550@gmail.com>
2024-07-11 21:37:50 +0200

    webstudio full

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const burger = document.querySelector('.header__burger');
 const headerClose = document.querySelector('.header__close');
 const headerMenu = document.querySelector('.header__menu');
-const headerContacts = document.querySelector('.header__nav__link__contacts');
+const heroButton = document.querySelector('.hero__button');
 const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.close');
+const navButton = document.querySelector('.nav__button');
 
 function onBurgerClick() {
   headerMenu.classList.toggle('is-hidden');
@@ -11,9 +12,13 @@ function onBurgerClick() {
 function onModalClick() {
   modal.classList.toggle('is-hidden');
 }
+function onNavButtonClick(){
+  navButton.classList.toggle('nav__button--current');
+}
 
 burger.addEventListener('click', onBurgerClick);
 headerClose.addEventListener('click', onBurgerClick);
 
-headerContacts.addEventListener('click', onModalClick);
+heroButton.addEventListener('click', onModalClick);
 modalClose.addEventListener('click', onModalClick);
+navButton.addEventListener('click', onNavButtonClick);
